7b932479b241fa1e2ade28aa6c4502db5beb15a9
yangxin <717147062@qq.com>
2020-07-02 16:34:21 +0800

    第三次提交

---
+++ src/main/java/com/example/gitproject/A.java
@@ -0,0 +1,13 @@
+package com.example.gitproject;
+
+/**
+ * @class: A
+ * @desc:
+ * @author: yx
+ * @date: 2020/7/2 16:31
+ */
+public class A {
+    public void aa(){
+        System.out.println("第三次提交");
+    }
+}
